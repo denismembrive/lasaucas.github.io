c51a2c2731d904e2d29b53ffa3701952eb71870a
denismembrive <100216369+denismembrive@users.noreply.github.com>
2023-12-29 11:04:04 -0300

    Primer commit

---
+++ Contacto/app.py
@@ -0,0 +1,131 @@
+# Importamos el metodo random para generar auto-legajo 
+import random
+#Importamos las clases para enviar el email
+import smtplib
+from email.message import EmailMessage
+
+from flask import (Flask, Response, jsonify, redirect, render_template,
+                   request, url_for)
+
+import database as dbase
+from product import Product
+
+db = dbase.dbConnection()
+
+app = Flask(__name__)
+
+#Rutas de la aplicación
+@app.route('/')
+def home():
+    products = db['products']
+    productsReceived = products.find()
+    return render_template('index.html', products = productsReceived)
+
+'''#Ruta home 2
+@app.route('/')
+def homeDos():
+    products = db['products']
+    productsReceived = products.find()
+    return render_template('index.html', products = productsReceived)
+'''
+# Legajo generado automaticamente
+
+orden = random.randint(2000, 4500)
+
+#Method Post
+@app.route('/products', methods=['POST'])
+def addProduct():
+    products = db['products']
+    name = request.form['name']
+    lastName = request.form['lastName']
+    dni = request.form['dni']
+    email = request.form['email']
+    pais = request.form['pais']
+    message = request.form['message']
+
+    if name and lastName and dni and email and pais and message and orden:
+        product = Product(name, lastName, dni, email, pais, message, orden)
+        products.insert_one(product.toDBCollection())
+        response = jsonify({
+            'name' : name,
+            'lasName': lastName,
+            'dni': dni,
+            'email': email,
+            'pais': pais,
+            'message' : message,
+            'orden': orden
+            
+            
+        })                                                                             # Alguno cambios
+        return redirect(url_for('home'))
+    else:
+        return notFound()
+    
+    
+    
+        
+        
+# Envio de email confirmando suscripcion
+
+remitente = "denismembrive4@gmail.com"
+destinatario = "denismembrive@gmail.com"
+mensaje = "¡Se ha registrado correctamente!"
+email = EmailMessage()
+email["From"] = remitente
+email["To"] = destinatario
+email["Subject"] = "Correo de prueba"
+email.set_content(mensaje)
+smtp = smtplib.SMTP_SSL("smtp.gmail.com")
+smtp.login(remitente, "znvqcqmcqwkptdfe")
+smtp.sendmail(remitente, destinatario, email.as_string())
+smtp.quit()
+
+
+
+
+
+
+
+
+
+
+
+#Method delete
+@app.route('/delete/<string:product_name>')
+def delete(product_name):
+    products = db['products']
+    products.delete_one({'name' : product_name})
+    return redirect(url_for('home'))
+
+#Method Put
+@app.route('/edit/<string:product_name>', methods=['POST'])
+def edit(product_name):
+    products = db['products']
+    name = request.form['name']
+    lastName = request.form['lastName']
+    dni = request.form['dni']
+    email = request.form['email']
+    pais = request.form['pais']
+    message = request.form['message']
+
+    if name and lastName and dni and email and pais and message:
+        products.update_one({'name' : product_name}, {'$set' : {'name' : name,'lastName' : lastName, 'dni' : dni, 'email' : email, 'pais' : pais,  'message' : message, }})
+        response = jsonify({'message' : 'Producto ' + product_name + ' actualizado correctamente'})
+        return redirect(url_for('home'))
+    else:
+        return notFound()
+
+@app.errorhandler(404)
+def notFound(error=None):
+    message ={
+        'message': 'No encontrado ' + request.url,
+        'status': '404 Not Found'
+    }
+    response = jsonify(message)
+    response.status_code = 404
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
---
+++ Contacto/product.py
@@ -0,0 +1,29 @@
+# Creamos la clase productos y la coleccion que guardaremos en nuestra base de datos
+class Product:
+    def __init__(self, name, lastName, dni, email, pais, message, orden):                                               # VAMOS A HACER UNOS CAMBIOS
+        self.name = name
+        self.lastName = lastName
+        self.dni = dni
+        self.email = email
+        self.pais = pais
+        self.message = message                                     
+        self.orden = orden
+
+
+# Creamos la funcion para crear la coleccion o el documento que se va a guardar en la base de datos
+
+    def toDBCollection(self):
+        return{
+            'name': self.name,
+            'lastName': self.lastName,
+            'dni': self.dni,
+            'email': self.email,
+            'pais': self.pais,
+            'message': self.message,
+            'orden': self.orden
+            
+            
+        }
+    
+
+     
